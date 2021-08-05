bcc80aa12fc7db3505f5f32dbd27a6ec60e659cb
Blidnight <jeansaintfleur34@gmail.com>
2021-08-05 15:29:35 -0400

    remove session

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -11,7 +11,6 @@ import { LobbyResolver, UserResolver } from './resolver';
 import * as express from 'express';
 import * as path from 'path';
 import * as http from 'http';
-import * as session from 'express-session';
 
 import errorFormater from './error-formater';
 
@@ -37,22 +36,12 @@ async function main() {
   const port = process.env.PORT || 4000;
 
   const corsOptions = {
-    credentials: true,
     origin:
       process.env.NODE_ENV === 'production'
         ? process.env.PROD_ORIGIN
         : process.env.DEV_ORIGIN,
   };
 
-  const sessionOption = {
-    secret: process.env.SESSION_SECRET as string,
-    cookie: {
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-      secure: process.env.NODE_ENV === 'production',
-    },
-  };
-
-  app.use(session(sessionOption as any));
   app.set('trust proxy', 1);
 
   server.applyMiddleware({ app: app as any, cors: corsOptions });
