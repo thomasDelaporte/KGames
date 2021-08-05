d9095dcc257bd112b57cd0ebf846762fda5e330a
Thomas Delaporte <thomas.delaporte30@gmail.com>
2021-08-05 19:02:56 +0200

    Notify clipboard

--- packages/web/src/pages/lobby/index.tsx
+++ packages/web/src/pages/lobby/index.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+
 import { GameResultSimple } from '@kgames/geoquizz';
 
 import './index.scss';
+import { motion } from 'framer-motion';
 
 export default function LobbyPage() {
     const [step, setStep] = useState(0);
@@ -18,34 +21,57 @@ export default function LobbyPage() {
     );
 }
 
-export const LobbyPlayers = ({ setStep }) => (
-    <div className="lobby__content">
-        <h2 className="lobby__content__title">Inviter vos amis</h2>
+export const LobbyPlayers = ({ setStep }) => {
 
-        <div className="lobby__players players">
-            <div className="players__item">
-                <img className="players__item__thumbnail" src="https://static.u.gg/assets/lol/riot_static/11.15.1/img/profileicon/4760.png" />
-                <h2>DLP</h2>
+    const [showCopy, setShowCopy] = useState(false);
+
+    useEffect(() => {
+
+        console.log(showCopy);
+        if(showCopy === false)
+            return;
+
+        navigator.clipboard.writeText(window.location.href)
+        setTimeout(() => setShowCopy(false), 1000)
+    }, [showCopy]);
+
+    return (
+        <div className="lobby__content">
+            <h2 className="lobby__content__title">Inviter vos amis</h2>
+
+            <div className="lobby__players players">
+                <div className="players__item">
+                    <img className="players__item__thumbnail" src="https://static.u.gg/assets/lol/riot_static/11.15.1/img/profileicon/4760.png" />
+                    <h2>DLP</h2>
+                </div>
+
+                <div className="players__item">
+                    <img className="players__item__thumbnail" src="https://static.u.gg/assets/lol/riot_static/11.15.1/img/profileicon/4750.png" />
+                    <h2>Fightek</h2>
+                </div>
+
+                <div className="players__item">
+                    <img className="players__item__thumbnail" src="https://static.u.gg/assets/lol/riot_static/11.15.1/img/profileicon/4730.png" />
+                    <h2>Fasyse</h2>
+                </div>
+
+                <button className="players__item players__item--empty" onClick={() => setShowCopy(true)}>
+                    <motion.span 
+                        style={{ x: '-50%' }}
+                        animate={{ y: showCopy ? '-1.75em' : '0.5rem', opacity: showCopy ? 1 : 0 }}
+                        transition={{ type: 'spring', stiffness: 150, delay: .1 }}
+                        className="players__item--empty__copy">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-16 10v-14h14v4h-10v10h-4z" fill="#ffffff"/></svg>Vous avez copié le lien
+                    </motion.span>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 7c2.761 0 5 2.239 5 5s-2.239 5-5 5-5-2.239-5-5 2.239-5 5-5zm11.122 12.065c-.073.301-.122.611-.122.935 0 2.209 1.791 4 4 4s4-1.791 4-4-1.791-4-4-4c-1.165 0-2.204.506-2.935 1.301l-5.488-2.927c-.23.636-.549 1.229-.943 1.764l5.488 2.927zm7.878-15.065c0-2.209-1.791-4-4-4s-4 1.791-4 4c0 .324.049.634.122.935l-5.488 2.927c.395.535.713 1.127.943 1.764l5.488-2.927c.731.795 1.77 1.301 2.935 1.301 2.209 0 4-1.791 4-4z"/></svg>Inviter vos amis
+                </button>
             </div>
 
-            <div className="players__item">
-                <img className="players__item__thumbnail" src="https://static.u.gg/assets/lol/riot_static/11.15.1/img/profileicon/4750.png" />
-                <h2>Fightek</h2>
-            </div>
-
-            <div className="players__item">
-                <img className="players__item__thumbnail" src="https://static.u.gg/assets/lol/riot_static/11.15.1/img/profileicon/4730.png" />
-                <h2>Fasyse</h2>
-            </div>
-
-            <button className="players__item players__item--empty" onClick={() => navigator.clipboard.writeText(window.location.href)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 7c2.761 0 5 2.239 5 5s-2.239 5-5 5-5-2.239-5-5 2.239-5 5-5zm11.122 12.065c-.073.301-.122.611-.122.935 0 2.209 1.791 4 4 4s4-1.791 4-4-1.791-4-4-4c-1.165 0-2.204.506-2.935 1.301l-5.488-2.927c-.23.636-.549 1.229-.943 1.764l5.488 2.927zm7.878-15.065c0-2.209-1.791-4-4-4s-4 1.791-4 4c0 .324.049.634.122.935l-5.488 2.927c.395.535.713 1.127.943 1.764l5.488-2.927c.731.795 1.77 1.301 2.935 1.301 2.209 0 4-1.791 4-4z"/></svg>Inviter vos amis
-            </button>
+            <button className="btn" onClick={() => setStep(1)}>Choissisez le jeu</button>
         </div>
-
-        <button className="btn" onClick={() => setStep(1)}>Choissisez le jeu</button>
-    </div>
-)
+    )
+}
 
 export const LobbyGames = ({ setStep }) => {
     const [selectedGame, setSelectedGame] = useState();
