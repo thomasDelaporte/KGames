0f74fc94ab7a1c76245482ea51e146cb43f2e111
Thomas Delaporte <thomas.delaporte30@gmail.com>
2022-01-03 15:09:32 +0100

    Clear lobbies

--- packages/server/src/services/LobbyService.ts
+++ packages/server/src/services/LobbyService.ts
@@ -38,4 +38,13 @@ export class LobbyService {
     public getLobbies(): Lobby[] {
         return Object.values(this.lobbies);
     }
+    
+    public clearLobbies() {
+
+        Object.values(this.lobbies).forEach(lobby => {
+            lobby.players.forEach(player => player.socket?.close);
+        });
+
+        this.lobbies = {};
+    }
 }
