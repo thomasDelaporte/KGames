02ac114dfda80f32dd8f78a6783bb1fda1005f72
Thomas Delaporte <thomas.delaporte30@gmail.com>
2022-01-21 12:58:13 +0100

    Fix geoquizz lowercase folder

--- packages/web/src/games/geoquizz/questions/Flag/QuestionFlag.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './QuestionFlag.style.scss';
-
-export const QuestionFlag = ({ room, number }: any) => {
-    return (
-        <img className="question-flag" src={import.meta.env.VITE_API + '/geoquizz/flag/' + room + '?' + number} />
-    )
-}
